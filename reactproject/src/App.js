f4a3d9a3261dc551e29484848eb24a49f5f50c27
Mohammad Abdul Hamid <abdulhamid529589@gmail.com>
2025-05-05 12:40:51 -0700

    add image and a card component

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import BoxModel from './Components/BoxModel/BoxModel';
-
+import Bg from './Components/Background/Bg.jsx'
+import RecipeReviewCard from './Components/RecipeReviewCard/RecipeReviewCard.jsx';
 
 function App() {
   return (
     <div className="App">
       <h1>Hellow</h1>
       <BoxModel/>
+      <Bg/>
+      <RecipeReviewCard/>
     </div>
   );
 }
