994aff7536e2208ec0e3f2a4d497f0509db857e3
abdulhamid529589@gmail.com <abdulhamid529589@gmail.com>
2025-05-23 23:12:52 +0600

    new feature

---
+++ practice_set2/src/Component/OutFeature/OurFeature.jsx
@@ -0,0 +1,10 @@
+import './OurFeature.css'
+
+export default function OurFeature() {
+  return (
+    <div className='title'>
+      <h1>Our Feature</h1>
+      <p>There is no one who loves itself, who seeks</p>
+    </div>
+  )
+}
